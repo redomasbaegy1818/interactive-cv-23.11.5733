4c45288b67663732ca73cde2378b959bc3e7b9ab
redomasbaegy1818 <redohardianathan77@gmail.com>
2025-07-04 17:04:24 +0700

    updateredo

---
+++ frontend/src/main.js
@@ -0,0 +1,9 @@
+import './assets/main.css';
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import animateOnScroll from './directives/animateOnScroll'; // <-- Impor
+const app = createApp(App);
+app.directive('animate-on-scroll', animateOnScroll); // <-- Daftarkan
+app.use(router);
+app.mount('#app');
